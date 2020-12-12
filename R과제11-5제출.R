4a8323e5de7f896b8a83c4cd86fa80b241d68c41
Alex Nam <greennam@naver.com>
2020-12-13 05:50:36 +0900

    201213 0550

--- R과제11-5제출.R
+++ R과제11-5제출.R
@@ -1,10 +1,17 @@
 # 1.
-head(state.x77)
-head(state.region)
-
-st.region <- split(state.x77, state.region)
-summary(st.region)
-class(st.region)
+st.region <-split(state.x77, state.region)
+st.region
 
 # 2.
-subset(state.x77, state.x77[,"Area"] > state.x77["Alabama","Area"] & state.x77[,"Area"] < state.x77["California","Area"])
+subset(state.x77, state.x77[,"Area"] > state.x77["Alabama","Area"] & 
+         state.x77[,"Area"] < state.x77["California","Area"],
+       select = c(Population, Income, Area))
+
+# 3. 
+idx <- sample(nrow(iris), size = 40, replace = F)
+idx
+iris.40 <- iris[idx,]
+
+#iris.110 <- 
+remain <- 1:150
+idx110 <- (remain == idx)
